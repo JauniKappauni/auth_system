448a25cb7097a45a5281df85cae829e265e36dbb
JauniKappauni <social.jauni@gmail.com>
2025-05-03 17:49:52 +0200

    Implement password reset functionality with email verification

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const session = require('express-session');
+const express = require('express')
+const session = require('express-session')
 const env = require('dotenv')
-require('dotenv').config();
-const mysql = require('mysql2');
+require('dotenv').config()
+const mysql = require('mysql2')
+const nodemailer = require('nodemailer')
+const crypto = require('crypto');
 const app = express();
 const port = 3000;
 
@@ -10,6 +12,10 @@ const dbhost = process.env.DB_HOST;
 const dbuser = process.env.DB_USER;
 const dbpassword = process.env.DB_PASSWORD;
 const dbname = process.env.DB_NAME;
+
+const mailhost = process.env.MAIL_HOST;
+const mailuser = process.env.MAIL_USER;
+const mailpassword = process.env.MAIL_PASSWORD;
 
 const conn = mysql.createConnection({
     host: dbhost,
@@ -93,6 +99,96 @@ app.post('/logout', (req, res) => {
         res.redirect("/")
     })
 })
+
+app.get('/forgot-password', (req, res) => {
+    res.render('forgot-password', { title: 'Forgot Password'});
+})
+
+app.post('/forgot-password' , (req, res) => {
+    const emailvalue = req.body.email
+    conn.query('SELECT * FROM users WHERE email = ?', [emailvalue], (err, results) => {
+        if(err){
+            console.error(err)
+            res.send('No email found in the database')
+        }
+        if(results.length == 0){
+            res.send("email not found")
+        }
+        else{
+            const reset_token = crypto.randomBytes(32).toString("hex")
+            const reset_expires = new Date(Date.now() + 3600000)
+            conn.query('UPDATE users SET reset_token = ?, reset_expires = ? WHERE email = ?', [reset_token, reset_expires, emailvalue], (err2) => {
+                if(err){
+                    console.error(err2)
+                    res.send('No email found in the database')
+                }
+                else{
+                    const transporter = nodemailer.createTransport({
+                        host: `${mailhost}`,
+                        port: 465,
+                        secure: true,
+                        auth: {
+                          user: `${mailuser}`,
+                          pass: `${mailpassword}`,
+                        },
+                      });
+                      
+                      async function email() {
+                        const info = await transporter.sendMail({
+                          from: `Jauni.de - Mail System" ${mailuser}`,
+                          to: `${emailvalue}`,
+                          subject: "Hello ✔",
+                          text: "Hello world?",
+                          html: `<a href=http://localhost:3000/reset-password?token=${reset_token}>Reset Password</a>`,
+                        });
+                      
+                        console.log("Message sent: %s", info.messageId);
+                      }
+                      
+                      email().catch(console.error);
+                    res.send("Check your email")
+                }
+            })
+        }})})
+
+app.get('/reset-password', (req,res) => {
+    const reset_token = req.query.token
+    if(!reset_token){
+        return res.send("token is missing")
+    }
+    else{
+        res.render('reset-password', {title: "Reset Password", token: reset_token})
+    }
+})
+app.post('/reset-password', (req, res) => {
+    const reset_token = req.body.token
+    const newPassword = req.body.password
+    conn.query('SELECT * FROM users WHERE reset_token = ?', [reset_token], (err, results) => {
+        if(err){
+            console.error(err)
+            res.send('No token found in the database')
+        }
+        if(results.length == 0){
+            res.send("token not found")
+        }
+        else{
+            const expirationDate = new Date(results[0].token_expires)
+                if(expirationDate.getTime() < Date.now()){
+                    res.send("Token expired")
+                }
+                else{
+                    conn.query('UPDATE users SET password = ?, reset_token = NULL, reset_expires = NULL WHERE reset_token = ?', [newPassword, reset_token], (err2) => {
+                        if(err2){
+                            console.error(err2)
+                            return res.send("Error updating password")
+                        }
+                        else{
+                            res.send("Passwort successfully changed")
+                        }
+                    })
+                }
+            }})})
+
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
